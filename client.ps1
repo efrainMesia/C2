30072243d33a4dbe1ee0d534a3006ee340b188fc
Efrain.mesia <efrain.mesia@gmail.com>
2022-07-26 11:43:33 +0300

    Powershell script disables firewall, updates packages and opens the client

--- client.ps1
+++ client.ps1
@@ -5,8 +5,8 @@
 ) #end param
 
 $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
-$currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-if($currentPrincipal){
+$runningAsAdmin = $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+if($runningAsAdmin){
   Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
   pip install -r requirements.txt
   $serverip_key = "--serverip=$serverip"
